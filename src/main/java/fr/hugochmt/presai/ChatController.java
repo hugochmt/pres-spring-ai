1d8145450c9a4087289786a3a67bb59fe90b35e1
hugochmt <h.chomarat@yahoo.fr>
2025-05-16 16:35:24 +0200

    init final chat implementation

---
+++ src/main/java/fr/hugochmt/presai/ChatController.java
@@ -0,0 +1,93 @@
+package fr.hugochmt.presai;
+
+import java.util.List;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+  private final ChatClient chatClient;
+
+  public ChatController(ChatClient.Builder chatClientBuilder) {
+    this.chatClient = chatClientBuilder.build();
+  }
+
+  @GetMapping("/ask-ai")
+  public String askAi(String userInput) {
+    return this.chatClient.prompt(userInput).call().content();
+  }
+
+  @GetMapping("/ask-perceval")
+  public String askAi2(String userInput) {
+    return this.chatClient
+        .prompt()
+        .system(
+            """
+          Tu es le personnage Perceval de la série Kaamelott.
+          Parle de la même manière que lui.
+          """)
+        .user(userInput)
+        .call()
+        .content();
+  }
+
+  /**
+   * Problèmes : - On doit fournir le format au modèle - pas sûr qu'il nous renvoie exactement
+   * l'objet
+   */
+  @GetMapping("/ask-perceval-potions")
+  public String askAi3(String userInput) {
+    return this.chatClient
+        .prompt()
+        .system(
+            """
+            Tu es le personnage Perceval de la série Kaamelott.
+            Parle de la même manière que lui.
+            """)
+        .system(
+            """
+            La recette de la potion doit avoir un nom, un pouvoir, et un effet.
+            Le résultat doit être au format json avec une clé pour chaque élément.
+            Retourne juste le contenu du json""")
+        .system(
+            "Je vais te donner un pouvoir. Invente une potion magique de Merlin pour avoir ce pouvoir")
+        .user(userInput)
+        .call()
+        .content();
+  }
+
+  @GetMapping("/ask-perceval-potions-structured")
+  public Potion askAi3StructuredOutput(String userInput) {
+    return this.chatClient
+        .prompt()
+        .system(
+            """
+            Tu es le personnage Perceval de la série Kaamelott.
+            Parle de la même manière que lui.
+            """)
+        .system(
+            "Je vais te donner un pouvoir. Invente une potion magique de Merlin pour avoir ce pouvoir")
+        .user(userInput)
+        .call()
+        .entity(Potion.class);
+  }
+
+  @GetMapping("/ask-perceval-potions-structured")
+  public List<Potion> askAi3StructuredOutputList(String userInput) {
+    return this.chatClient
+        .prompt()
+        .system(
+            """
+            Tu es le personnage Perceval de la série Kaamelott.
+            Parle de la même manière que lui.
+            """)
+        .system(
+            "Je vais te donner un pouvoir. Invente une liste de 3 potions magiques de Merlin pour avoir ce pouvoir")
+        .user(userInput)
+        .call()
+        .entity(new ParameterizedTypeReference<>() {});
+  }
+}
---
+++ src/main/java/fr/hugochmt/presai/Potion.java
@@ -0,0 +1,3 @@
+package fr.hugochmt.presai;
+
+public record Potion(String nom, String pouvoir, String effetSecondaire) {}
