1d1c442b282280524f4ed37f509d56886263f646
hugochmt <h.chomarat@yahoo.fr>
2025-05-21 18:06:15 +0200

    add classify endpoint

--- ollama-model/src/main/java/fr/hugochmt/presai/ollama/LocalChatController.java
+++ ollama-model/src/main/java/fr/hugochmt/presai/ollama/LocalChatController.java
@@ -1,14 +1,31 @@
 package fr.hugochmt.presai.ollama;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LocalChatController {
 
-  @GetMapping("/local-ai")
-  public ResponseEntity<String> askAi1(String userInput) {
-    return ResponseEntity.ok("hello");
+  private final ChatClient chatClient;
+
+  public LocalChatController(ChatClient.Builder builder) {
+    this.chatClient = builder.build();
+  }
+
+  @GetMapping("/ai")
+  public String askAi1(String userInput) {
+    return this.chatClient.prompt(userInput).call().content();
+  }
+
+  @GetMapping("/classify")
+  public String askAi2(String userInput) {
+    return this.chatClient
+        .prompt()
+        .system(
+            "Classify the provided character. If the character is a bad guy return 'BAD_GUY', if it is the good guy in many stories return 'GOOD_GUY'")
+        .user(userInput)
+        .call()
+        .content();
   }
 }
